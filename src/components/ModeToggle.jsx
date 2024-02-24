1e00642519b4bd7a7fb832e7bf37f51ae7654140
elyulidev <elyuli.dev@gmail.com>
2024-02-25 01:27:29 +0200

    Organize imports ModeToggle.jsx

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { FaSun, FaMoon } from "react-icons/fa";
 import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
+import { Button } from "./ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "./ui/dropdown-menu";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const ModeToggle = () => {
 	const { setTheme } = useTheme();
