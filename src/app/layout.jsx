78249cf76f3e115e4d346e0697a746303eaf459b
elyulidev <elyuli.dev@gmail.com>
2024-02-25 01:35:26 +0200

    Add to html tag suppressHydrationWarning

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='pt'>
+		<html lang='pt' suppressHydrationWarning>
 			<body
 				className={cn(
 					"min-h-screen bg-background font-sans antialiased overflow-x-hidden",
