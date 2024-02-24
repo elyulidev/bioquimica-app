b7abd13aaa1c7be8ed41edddc8a9e96e326fb5ac
elyulidev <elyuli.dev@gmail.com>
2024-02-25 01:04:55 +0200

    Change language to pt

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='en'>
+		<html lang='pt'>
 			<body
 				className={cn(
 					"min-h-screen bg-background font-sans antialiased overflow-x-hidden",
