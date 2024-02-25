132e632bd8438b240db145894d28126b63814faa
elyulidev <elyuli.dev@gmail.com>
2024-02-25 02:54:29 +0200

    ModeToggle: Fix imports

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
+} from "@/components/ui/dropdown-menu";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const ModeToggle = () => {
@@ -16,7 +16,7 @@ const ModeToggle = () => {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant='outline' size='icon'>
+				<Button variant='custom' size='icon'>
 					<FaSun className='h-[1.2rem] w-[1.2rem] text-primary rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 ' />
 					<FaMoon className='absolute h-[1.2rem] w-[1.2rem] text-primary rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
 					<span className='sr-only'>Toggle theme</span>
