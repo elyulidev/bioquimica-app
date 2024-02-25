5daf6a216ffb63b1a523d3d326cd358cfc62ba12
elyulidev <elyuli.dev@gmail.com>
2024-02-26 01:51:02 +0200

    Change logic of useState and useEffect to ThemeProvider.jsx

--- src/components/SwitchComp.jsx
+++ src/components/SwitchComp.jsx
@@ -1,21 +1,10 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import { Switch } from "./ui/switch";
 
 const SwitchComp = () => {
-	const [mounted, setMounted] = useState(false);
 	const { setTheme, resolvedTheme } = useTheme();
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	if (!mounted) return null;
-
-	console.log("mounted>>>", mounted);
-	console.log("resolvedTheme>>>", resolvedTheme);
 
 	const toogleMode = () => {
 		setTheme(resolvedTheme === "light" ? "dark" : "light");
